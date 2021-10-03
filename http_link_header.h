c16f65619633de440a989486c262582a4d6d679e
Daniel X. Pape <dpape@dpape.com>
2021-10-02 20:10:45 -0700

    add parse() for single header string and vector of header strings; only does simple splitting on comma chars

--- http_link_header.h
+++ http_link_header.h
@@ -2,9 +2,91 @@
 #define HTTP_LINK_HEADER_H
 
 #include <string>
+#include <set>
+#include <vector>
+#include <sstream>
 
-std::string hello() {
-    return "hello";
+namespace http_link_header {
+
+    namespace detail {
+
+        std::vector<std::string> split(const std::string& s, char delimiter)
+        {
+            std::vector<std::string> tokens;
+            std::string token;
+            std::istringstream stream(s);
+            while (std::getline(stream, token, delimiter))
+                tokens.push_back(token);
+            return tokens;
+        }
+
+    }
+
+    class TargetAttribute {
+    public:
+        std::string name;
+        std::string value;
+    };
+
+    class Link {
+    public:
+        std::string linkContext;
+        std::vector<std::string> linkRelations;
+        std::string linkTarget;
+        std::vector<TargetAttribute> targetAttributes;
+    };
+
+    /**
+     * Parses zero or more comma-separated link-values from a Link header field
+     *
+     * @param linkFieldValue string containing the value of a Link header field
+     *
+     * @return vector of zero or more Link objects
+     */
+    std::vector<Link> parse(const std::string& linkFieldValue) {
+        std::vector<Link> links;
+
+        if(linkFieldValue.empty())
+            return links;
+
+        // split up linkFieldValue string into multiple header strings if necessary
+
+        if(linkFieldValue.find("Link: ") != 0)
+            return links;
+
+        for(auto s : detail::split(linkFieldValue, ',')) {
+            Link link{};
+            links.push_back(link);
+        }
+
+        return links;
+    }
+
+    /**
+     * Parse the Link header fields that a HTTP header set contains
+     *
+     * @param headers vector of HTTP header strings
+     *
+     * @return vector of zero or more Link objects
+     */
+    std::vector<Link> parse(const std::vector<std::string>& headers) {
+        std::vector<Link> links;
+
+        if(headers.empty())
+            return links;
+
+        for(const auto& header : headers) {
+            if (header.find("Link: ") != 0)
+                continue;
+            std::vector<Link> headerLinks = parse(header);
+            links.insert(links.end(), headerLinks.begin(), headerLinks.end());
+        }
+
+        return links;
+    }
+
+
 }
+
 
 #endif //HTTP_LINK_HEADER_H
--- tests.cpp
+++ tests.cpp
@@ -3,7 +3,62 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("testing the hello function") {
-    CHECK(hello() == "hello");
+#include <typeinfo>
+
+std::string header_previousChapter =
+        R"(Link: <http://example.com/TheBook/chapter2>; rel="previous"; title="previous chapter")";
+
+std::string header_nextChapter =
+        R"(, <http://example.com/TheBook/chapter4>; rel="next"; title="next chapter")";
+
+TEST_CASE("parsing empty header string returns empty vector") {
+    CHECK(http_link_header::parse("").empty());
+}
+
+TEST_CASE("parsing non-Link header string returns empty vector") {
+    CHECK(http_link_header::parse("Subject: hello").empty());
+}
+
+TEST_CASE("parsing Link header string returns vector with one Link") {
+    CHECK(http_link_header::parse(header_previousChapter).size() == 1);
+}
+
+TEST_CASE("parsing Link header string returns vector with two Links") {
+    std::string header = header_previousChapter + header_nextChapter;
+    CHECK(http_link_header::parse(header).size() == 2);
+}
+
+
+
+TEST_CASE("parsing empty vector of header strings returns empty vector of Links") {
+    std::vector<std::string> headers;
+    CHECK(http_link_header::parse(headers).empty());
+}
+
+TEST_CASE("parsing vector of header strings containing non-Link header string returns empty vector") {
+    std::vector<std::string> headers;
+    headers.emplace_back("Subject: hello");
+    CHECK(http_link_header::parse(headers).empty());
+}
+
+TEST_CASE("parsing vector of header strings with one Link header returns vector with one Link") {
+    std::vector<std::string> headers;
+    headers.push_back(header_previousChapter);
+    CHECK(http_link_header::parse(headers).size() == 1);
+}
+
+TEST_CASE("parsing vector of header strings with one Link header string returns vector with two Links") {
+    std::string header = header_previousChapter + header_nextChapter;
+    std::vector<std::string> headers;
+    headers.push_back(header);
+    CHECK(http_link_header::parse(headers).size() == 2);
+}
+
+TEST_CASE("parsing vector of header strings with two Link header string sreturns vector with four Links") {
+    std::string header = header_previousChapter + header_nextChapter;
+    std::vector<std::string> headers;
+    headers.push_back(header);
+    headers.push_back(header);
+    CHECK(http_link_header::parse(headers).size() == 4);
 }
 
